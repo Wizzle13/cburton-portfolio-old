d62c2f4c86176e74936583a6072dca08eeab0472
CBurton <burton.chris13@gmail.com>
2022-07-16 13:36:01 -0500

    update sections

---
+++ src/components/Resume/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Resume() {
+<div>
+            <section id="aboutme">
+                <h2 class="section-title">Resume</h2>
+                <p class="resume">
+                    Download my <a href="../../assests/CBurtonResume.pdf">Resume</a>
+                 Front-end proficiencies
+                 Back-end proficiencies
+                </p>
+            </section>
+        </div>
+}
+
+export default Resume;
