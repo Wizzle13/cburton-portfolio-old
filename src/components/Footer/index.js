6aee456d4adf3040774ea76e19c6948b214e35f9
CBurton <burton.chris13@gmail.com>
2022-07-16 20:56:39 -0500

    Updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../../css/footer.css';
 
 function Footer() {
     return (
-        <footer>
-            <a href="https://github.com/Wizzle13">
-                <img src={require(`../../assets/images/Github.png`)} alt="Github" width='100' />
-            </a>
-            <a href="https://www.linkedin.com/in/christopher-burton-aba9651b9/">
-                <img src={require(`../../assets/images/Linkedin.png`)} alt="LinkedIn" width='100'/>
-            </a>
+        <footer >
+            <div id="iconLink">
+                <a href="https://github.com/Wizzle13">
+                    <img src={require(`../../assets/images/Github.png`)} alt="Github" width='100' />
+                </a>
+                <a href="https://www.linkedin.com/in/christopher-burton-aba9651b9/">
+                    <img src={require(`../../assets/images/Linkedin.png`)} alt="LinkedIn" width='100'/>
+                </a>
+            </div>
         </footer>
     )
 }
